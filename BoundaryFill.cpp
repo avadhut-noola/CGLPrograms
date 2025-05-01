5c372634067674ffc231d90092cdc9c386a1da0c
Avadhut Noola <avdhutnula@gmail.com>
2025-05-01 23:26:28 +0530

    Updated: boundary fill

--- BoundaryFill.cpp
+++ BoundaryFill.cpp
@@ -1,138 +1,61 @@
-#include <GL/glut.h>
 #include <iostream>
-#include <stack>
+#include <math.h>
+#include <time.h>
+#include <GL/glut.h>
+
 using namespace std;
 
-int winWidth = 640, winHeight = 480;
-float fillColor[3] = {0.0, 1.0, 0.0};   // Fill color (Green)
-float borderColor[3] = {0.0, 0.0, 0.0}; // Border color (Black)
-float bgColor[3] = {1.0, 1.0, 1.0};     // Background color (White)
-
-struct Point {
-    int x, y;
-    Point(int x = 0, int y = 0) : x(x), y(y) {}
-};
-
-// Sample polygon (triangle)
-Point poly[] = {{100, 100}, {300, 300}, {100, 300}};
-int polySize = 3;
-
-void drawPoly() {
-    glColor3fv(borderColor);
-    glBegin(GL_LINE_LOOP);
-    for (int i = 0; i < polySize; i++)
-        glVertex2i(poly[i].x, poly[i].y);
-    glEnd();
-    glFlush();
-}
-
-void getPixelColor(int x, int y, float* color) {
-    glReadPixels(x, winHeight - y, 1, 1, GL_RGB, GL_FLOAT, color);
-}
-
-void setPixelColor(int x, int y, float* color) {
-    glColor3fv(color);
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
-    glFlush();
-}
-
-bool compareColor(float* c1, float* c2) {
-    return (c1[0] == c2[0] && c1[1] == c2[1] && c1[2] == c2[2]);
-}
-
-void boundaryFill(int x, int y) {
-    stack<Point> pixels;
-    pixels.push(Point(x, y));
-    
-    while (!pixels.empty()) {
-        Point p = pixels.top();
-        pixels.pop();
-        
-        float color[3];
-        getPixelColor(p.x, p.y, color);
-        
-        if (!compareColor(color, borderColor) && !compareColor(color, fillColor)) {
-            setPixelColor(p.x, p.y, fillColor);
-            
-            pixels.push(Point(p.x + 1, p.y));
-            pixels.push(Point(p.x - 1, p.y));
-            pixels.push(Point(p.x, p.y + 1));
-            pixels.push(Point(p.x, p.y - 1));
-        }
-    }
-}
-
-void mouseClick(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        boundaryFill(x, y);
-    }
-}
-
-void keyboard(unsigned char key, int x, int y) {
-    if (key == 'f' || key == 'F') {
-        boundaryFill(winWidth/2, winHeight/2);
-    } else if (key == 27) { // ESC key
-        exit(0);
-    }
-}
-
-void menuFunc(int option) {
-    switch(option) {
-        case 1: // Fill with green
-            fillColor[0] = 0.0; fillColor[1] = 1.0; fillColor[2] = 0.0;
-            break;
-        case 2: // Fill with red
-            fillColor[0] = 1.0; fillColor[1] = 0.0; fillColor[2] = 0.0;
-            break;
-        case 3: // Fill with blue
-            fillColor[0] = 0.0; fillColor[1] = 0.0; fillColor[2] = 1.0;
-            break;
-        case 4: // Clear
-            glClear(GL_COLOR_BUFFER_BIT);
-            drawPoly();
-            break;
-        case 5: // Exit
-            exit(0);
-            break;
-    }
-}
-
-void display() {
-    glClearColor(bgColor[0], bgColor[1], bgColor[2], 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    drawPoly();
-    glFlush();
-}
-
-void init() {
+void init(){
+    glClearColor(1.0,1.0,1.0,0.0);
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, winWidth, 0, winHeight);
+    gluOrtho2D(0,640,0,480);
 }
 
-int main(int argc, char** argv) {
+void bound_it(int x, int y, float* fillColor, float* bc){
+    float color[3];
+    glReadPixels(x,y,1.0,1.0,GL_RGB,GL_FLOAT,color);
+    if((color[0]!=bc[0] || color[1]!=bc[1] || color[2]!=bc[2])&&(
+     color[0]!=fillColor[0] || color[1]!=fillColor[1] || color[2]!=fillColor[2])){
+        glColor3f(fillColor[0],fillColor[1],fillColor[2]);
+        glBegin(GL_POINTS);
+            glVertex2i(x,y);
+        glEnd();
+        glFlush();
+        bound_it(x+2,y,fillColor,bc);
+        bound_it(x-2,y,fillColor,bc);
+        bound_it(x,y+2,fillColor,bc);
+        bound_it(x,y-2,fillColor,bc);
+    }
+}
+
+void mouse(int btn, int state, int x, int y){
+            float bCol[] = {0,0,1};
+            float color[] = {0,0,1};
+            bound_it(x,y,color,bCol);
+}
+
+void world(){
+    glLineWidth(3);
+    glPointSize(2);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0,0,1);
+    glBegin(GL_LINE_LOOP);
+        glVertex2i(150,100);
+        glVertex2i(300,300);
+        glVertex2i(450,100);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(winWidth, winHeight);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Boundary Fill Algorithm");
-    
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(200,200);
+    glutCreateWindow("Many Amaze Very GL WOW");
     init();
-    glutDisplayFunc(display);
-    glutMouseFunc(mouseClick);
-    glutKeyboardFunc(keyboard);
-    
-    // Create menu
-    int menu = glutCreateMenu(menuFunc);
-    glutAddMenuEntry("Fill Green", 1);
-    glutAddMenuEntry("Fill Red", 2);
-    glutAddMenuEntry("Fill Blue", 3);
-    glutAddMenuEntry("Clear", 4);
-    glutAddMenuEntry("Exit", 5);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-    
+    glutDisplayFunc(world);
+    glutMouseFunc(mouse);
     glutMainLoop();
     return 0;
 }
